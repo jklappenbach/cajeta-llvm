436a7fd647c5f4585d39d18e371f08c8662facec
Julian Klappenbach <julian@twilight.digital>
2026-06-03 00:40:02 -0400

    [SPIRV] Lower SPV_KHR_ray_query operations via llvm.spv intrinsics
    Initialize/Proceed/GetIntersectionType emit OpRayQuery* opcodes. The __spirv_*
    builtin path is shader-gated (OpenCL-only) and cannot reach ray query (EnvVulkan-
    only), so the ops use llvm.spv.ray.query.* intrinsics + GlobalISel selection.

--- llvm/lib/Target/SPIRV/SPIRVInstructionSelector.cpp
+++ llvm/lib/Target/SPIRV/SPIRVInstructionSelector.cpp
@@ -444,6 +444,7 @@ private:
   bool selectGatherIntrinsic(Register &ResVReg, SPIRVTypeInst ResType,
                              MachineInstr &I) const;
   bool selectImageWriteIntrinsic(MachineInstr &I) const;
+  bool selectRayQueryInitialize(MachineInstr &I) const;
   bool selectResourceGetPointer(Register &ResVReg, SPIRVTypeInst ResType,
                                 MachineInstr &I) const;
   bool selectPushConstantGetPointer(Register &ResVReg, SPIRVTypeInst ResType,
@@ -1592,6 +1593,17 @@ bool SPIRVInstructionSelector::selectSincos(Register ResVReg,
     return true;
   }
   return false;
+}
+
+bool SPIRVInstructionSelector::selectRayQueryInitialize(MachineInstr &I) const {
+  // Void side-effecting G_INTRINSIC: operand 0 = intrinsic id, operands 1..8 =
+  // rq (variable ptr), accel, rayFlags, cullMask, origin, tMin, dir, tMax.
+  auto MIB = BuildMI(*I.getParent(), I, I.getDebugLoc(),
+                     TII.get(SPIRV::OpRayQueryInitializeKHR));
+  for (unsigned i = 1; i < I.getNumOperands(); ++i)
+    MIB.addUse(I.getOperand(i).getReg());
+  MIB.constrainAllUses(TII, TRI, RBI);
+  return true;
 }
 
 bool SPIRVInstructionSelector::selectOpWithSrcs(Register ResVReg,
@@ -4710,6 +4722,15 @@ bool SPIRVInstructionSelector::selectIntrinsic(Register ResVReg,
       report_fatal_error("incompatible result and operand types in a bitcast");
     return selectOpWithSrcs(ResVReg, ResType, I, {OpReg}, SPIRV::OpBitcast);
   }
+  case Intrinsic::spv_ray_query_initialize:
+    return selectRayQueryInitialize(I);
+  case Intrinsic::spv_ray_query_proceed:
+    return selectOpWithSrcs(ResVReg, ResType, I, {I.getOperand(2).getReg()},
+                            SPIRV::OpRayQueryProceedKHR);
+  case Intrinsic::spv_ray_query_get_intersection_type:
+    return selectOpWithSrcs(ResVReg, ResType, I,
+                            {I.getOperand(2).getReg(), I.getOperand(3).getReg()},
+                            SPIRV::OpRayQueryGetIntersectionTypeKHR);
   case Intrinsic::spv_unref_global:
   case Intrinsic::spv_init_global: {
     MachineInstr *MI = MRI->getVRegDef(I.getOperand(1).getReg());
